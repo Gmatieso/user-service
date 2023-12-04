1dad752c344fb9e02d0e9be5c3ef035362bf1361
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-12-04 20:28:11 +0300

    saveUser, getUserWithDepartment method and PostMapping and GetMapping done

--- src/main/java/com/techg/user/controller/UserController.java
+++ src/main/java/com/techg/user/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.techg.user.controller;
 
+import com.techg.user.VO.ResponseTemplateVO;
+import com.techg.user.entity.User;
+import com.techg.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
 public class UserController {
+    //create an object of userService
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user){
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment( @PathVariable("id") Long userId){
+        log.info("Inside getUserWithDepartment of UserController");
+        return userService.getUserWithDepartment(userId);
+    }
 }
